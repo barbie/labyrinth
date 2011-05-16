cfa70838e8bf65a373858b5bc551e857ddd6a8bd
Barbie <barbie@missbarbell.co.uk>
2011-05-16 08:21:00 +0100

    CGI & TT abstraction packages

---
+++ lib/Labyrinth/Query/CGI.pm
@@ -0,0 +1,90 @@
+package Labyrinth::Query::CGI;
+
+use warnings;
+use strict;
+
+my $VERSION = '5.06';
+
+=head1 NAME
+
+Labyrinth::Query::CGI - Environment hander for Labyrinth.
+
+=head1 SYNOPSIS
+
+  use Labyrinth::Query::CGI;
+  my $cgi = Labyrinth::Query::CGI->new();
+
+  $cgi->env();
+  $cgi->Vars();
+
+=head1 DESCRIPTION
+
+Thin wrapper around CGI.pm.
+
+=cut
+
+# -------------------------------------
+# Library Modules
+
+use base qw(CGI);
+
+# -------------------------------------
+# The Subs
+
+=head1 METHODS
+
+=over 4
+
+=item new
+
+Object constructor.
+
+=item env
+
+Provides the %ENV hash.
+
+=cut
+
+sub new {
+    my($class) = @_;
+    CGI::initialize_globals();
+
+    my $self = bless {
+        env         => \%ENV,
+    }, $class;
+
+    $self->SUPER::init;
+
+    $self;
+}
+
+sub env {
+    my $self = shift;
+    return $self->{env};
+}
+
+1;
+
+__END__
+
+=back
+
+=head1 SEE ALSO
+
+  CGI,
+  Labyrinth
+
+=head1 AUTHOR
+
+Barbie, <barbie@missbarbell.co.uk> for
+Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
+
+=head1 COPYRIGHT & LICENSE
+
+  Copyright (C) 2002-2011 Barbie for Miss Barbell Productions
+  All Rights Reserved.
+
+  This module is free software; you can redistribute it and/or
+  modify it under the Artistic License 2.0.
+
+=cut
---
+++ lib/Labyrinth/Writer/Parser/TT.pm
@@ -0,0 +1,113 @@
+package Labyrinth::Writer::Parser::TT;
+
+use warnings;
+use strict;
+
+my $VERSION = '5.06';
+
+=head1 NAME
+
+Labyrinth::Writer::Parser::TT - Provides template parsing with Template Toolkit
+
+=head1 SYNOPSIS
+
+  use Labyrinth::Writer::Parser::TT;
+  my $tt = Labyrinth::Writer::Parser::TT->new();
+  $tt->parser('mytemplate.html');
+
+=head1 DESCRIPTION
+
+This package provides the ability to parse a given template, with a given set
+of template variables using Template Toolkit.
+
+=cut
+
+# -------------------------------------
+# Library Modules
+
+use Template;
+
+use Labyrinth::Audit;
+use Labyrinth::Variables;
+
+# -------------------------------------
+# Variables
+
+my %config = (                              # default config info
+    RELATIVE        => 1,
+    ABSOLUTE        => 1,
+    INTERPOLATE     => 0,
+    POST_CHOMP      => 1,
+    TRIM            => 1,
+);
+
+# -------------------------------------
+# The Subs
+
+=head1 METHODS
+
+=over 4
+
+=item new
+
+Object constructor.
+
+=item parser( $template, $variables )
+
+Parses a given template, via Template Toolkit. Returns a string of the 
+parsed template.
+
+=cut
+
+sub new {
+    my($class) = @_;
+
+    my $self = bless { config => \%config }, $class;
+    $self;
+}
+
+sub parser {
+    my ($self, $layout, $vars) = @_;
+    my $path = $settings{'templates'};
+    my $output;
+
+#    use Data::Dumper;
+#    LogDebug( "layout=[$layout]" );
+#    LogDebug( "vars=".Dumper($vars) );
+
+    $self->{config}{INCLUDE_PATH} = $path;
+    $self->{config}{EVAL_PERL}    = ($vars->{evalperl} ? 1 : 0);
+    $self->{config}{OUTPUT_PATH}  = $vars->{cache};
+
+    my $parser = Template->new($self->{config});        # initialise parser
+    eval { $parser->process($layout,$vars,\$output) };
+    die $parser->error()    if($@ || !$output);
+
+    return \$output;
+}
+
+1;
+
+__END__
+
+=back
+
+=head1 SEE ALSO
+
+  Template (Template Toolkit)
+  Labyrinth
+
+=head1 AUTHOR
+
+Barbie, <barbie@missbarbell.co.uk> for
+Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
+
+=head1 COPYRIGHT & LICENSE
+
+  Copyright (C) 2002-2011 Barbie for Miss Barbell Productions
+  All Rights Reserved.
+
+  This module is free software; you can redistribute it and/or
+  modify it under the Artistic License 2.0.
+
+=cut
